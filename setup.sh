71b90675fb62b7aa88a15aa524c6883cdf557efd
Batresh <bjoernliborius@gmail.com>
2019-07-15 03:09:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
-apt update -y
 apt install -y i3
 #dependencies for i3-gaps
 apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool libxcb-shape0-dev libxcb-xrm-dev
