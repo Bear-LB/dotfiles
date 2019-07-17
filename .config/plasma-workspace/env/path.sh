4ed7c9234f555f20f87a6085bcaf78ff6ef8e7ab
Batresh <bjoernliborius@gmail.com>
2019-07-17 08:25:20 +0200

    Update path.sh

--- .config/plasma-workspace/env/path.sh
+++ .config/plasma-workspace/env/path.sh
@@ -2,5 +2,5 @@
 export PATH="$PATH:$(du "$HOME/.local/bin/" | cut -f2 | tr '\n' ':' | sed 's/:*//')"
 export TERMINAL="termite"
 export EDITOR="nvim"
-export FILE="vifm"
+export FILE="~/.config/vifm/scripts/vifmrun"
 export BROWSER="firefox"
