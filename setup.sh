a4b4d5c103c92872b7ed8736f40e46613badbd04
Batresh <bjoernliborius@gmail.com>
2019-09-08 16:03:14 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,3 +41,6 @@ chmod +x strap.sh
 ./strap.sh
 #add .zshrc config file for root user too.
 cp /home/$name/.zshrc ~/.zshrc
+# Make pacman and yay colorful and adds eye candy on the progress bar because why not.
+grep "^Color" /etc/pacman.conf >/dev/null || sed -i "s/^#Color/Color/" /etc/pacman.conf
+grep "ILoveCandy" /etc/pacman.conf >/dev/null || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
