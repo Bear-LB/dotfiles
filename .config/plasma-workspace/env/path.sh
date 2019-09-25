a6f26f8bf05c1dc74e9ff26ddbbae030fb90f12c
Batresh <bjoernliborius@gmail.com>
2019-09-25 23:46:54 +0200

    Delete path.sh

--- .config/plasma-workspace/env/path.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export PATH="$PATH:$(du "$HOME/.local/bin/" | cut -f2 | tr '\n' ':' | sed 's/:*//')"
-export TERMINAL="termite"
-export EDITOR="nvim"
-export FILE="$HOME/.config/vifm/scripts/vifmrun"
-export BROWSER="firefox"
