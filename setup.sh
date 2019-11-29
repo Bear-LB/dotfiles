e9c18babb0dde3b61a704d35c7da640de2acb31b
Batresh <bjoernliborius@gmail.com>
2019-11-29 13:54:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -96,6 +96,8 @@ unzip /tmp/Cousine.zip
 rm /tmp/*Windows*
 mkdir /usr/share/fonts/inconsolata && mkdir /usr/share/fonts/cousine/
 cp /tmp/Cousine* /usr/share/fonts/cousine/ && cp /tmp/Inconsolata* /usr/share/fonts/inconsolata/
+# Ohmyzsh
+sudo -u $name sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Clone dotfiles to home
 dir=$(mktemp -d)
 git clone -b master --depth 1 https://github.com/Batresh/dotfiles "$dir/gitrepo"
