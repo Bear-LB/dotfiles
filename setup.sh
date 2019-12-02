340dcafb51d34febc72587c1197fc49ad58f4583
Batresh <bjoernliborius@gmail.com>
2019-12-02 13:15:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -133,7 +133,7 @@ sudo -u bear wpg -ta /home/$name/.config/i3/config
 sudo -u bear wpg -ta /home/$name/.config/polybar/config
 # Lightdm
 pacman --noconfirm -S lightdm lightdm-webkit2-greeter
-pacman --noconfirm -S lightdm-66serv && pacman --noconfirm -S dbus-66serv consolekit2 consolekit-66serv networkmanager-66serv
+pacman --noconfirm -S lightdm-66serv && pacman --noconfirm -S dbus-66serv consolekit2 consolekit-66serv networkmanager-66serv dhclient-66serv
 sudo -u "$name" yay -S --noconfirm lightdm-webkit-theme-aether
 # s6 and s6-rc
 pacman --noconfirm -S boot-user@-66mod
