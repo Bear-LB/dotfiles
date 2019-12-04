957f82bb7d131a855e3f8f479de174c738d79e2f
Batresh <bjoernliborius@gmail.com>
2019-12-04 19:36:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,12 +58,10 @@ pacman --noconfirm -S firefox mpv neovim gnome-keyring exfat-utils dosfstools nt
 # Bloat Software 4
 pacman --noconfirm -S sxiv pulseaudio pulseaudio-alsa pulsemixer xsettingsd lxappearance scrot nitrogen
 # Bloat Software 5
-pacman --noconfirm -S xorg-font-utils streamlink wget adobe-source-han-sans-jp-fonts playerctl
+pacman --noconfirm -S xorg-font-utils streamlink wget adobe-source-han-sans-jp-fonts playerctl i3-gaps
 # Systemd software ?!
 pacman --noconfirm -S mpd || sudo -u "$name" yay -S --noconfirm mpd-light
 pacman --noconfirm -S transmission-cli && sudo -u "$name" yay -S --noconfirm stig
-# i3-Gaps
-sudo -u "$name" yay -S --noconfirm i3-gaps-next-git
 # Install zsh theme
 git clone https://github.com/romkatv/powerlevel10k.git /opt/powerlevel10k
 # Install oh-my-zsh --------------------------------------------- WORKS IN PROGRESS
