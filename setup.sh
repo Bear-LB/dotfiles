301d7ecf84b7065946bba31e61487d6c237e95b9
Batresh <bjoernliborius@gmail.com>
2019-12-15 03:37:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -61,7 +61,7 @@ pacman --noconfirm -S sxiv pulseaudio pulseaudio-alsa pulsemixer xsettingsd lxap
 # Bloat Software 5
 pacman --noconfirm -S streamlink adobe-source-han-sans-jp-fonts playerctl i3-gaps lightdm lightdm-webkit2-greeter || { echo 'failed at installing packages from official repo' ; exit 1; }
 # Bloat Software 6
-pacman --noconfirm -S unclutter || { echo 'failed at installing packages from official repo' ; exit 1; }
+pacman --noconfirm -S unclutter inotify-tools || { echo 'failed at installing packages from official repo' ; exit 1; }
 # Systemd software ?!
 pacman --noconfirm -S mpd || sudo -u "$name" yay -S --noconfirm mpd-light
 pacman --noconfirm -S transmission-cli && sudo -u "$name" yay -S --noconfirm stig
