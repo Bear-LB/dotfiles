26c56b70774e2b3acf9c1b938d9af51995a9d3d1
Batresh <bjoernliborius@gmail.com>
2020-02-10 20:45:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,42 +1,31 @@
-#------------------------------------------------------------
-# THINGS TO MODIFY BEFORE DEPLOYMENT
-# Your username
-name="YOURUSERNAME"
-# Your user password
-pass="YOURPASSWORD"
-# Running in VMWare ?
-VMWAREGUEST=${VMWAREGUEST:-no}
-#-------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Refresh Arch keyrings
+pacman --noconfirm -Sy archlinux-keyring
+# Upgrade
+pacman --noconfirm -Syu
+pacman --noconfirm --needed -S base-devel linux-firmware diffutils vim networkmanager man-db man-pages texinfo exfat-utils e2fsprogs dialog
+newperms() {
+	sed -i "/#Deploydot/d" /etc/sudoers
+	echo "$* #Deploydot" >> /etc/sudoers ;}
+newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
+getuserandpass() { \
+	name=$(dialog --inputbox "First, please enter a name for the user account." 10 60 3>&1 1>&2 2>&3 3>&1) || exit
+	while ! echo "$name" | grep "^[a-z_][a-z0-9_-]*$" >/dev/null 2>&1; do
+		name=$(dialog --no-cancel --inputbox "Username not valid. Give a username beginning with a letter, with only lowercase letters, - or _." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done
+	pass1=$(dialog --no-cancel --passwordbox "Enter a password for that user." 10 60 3>&1 1>&2 2>&3 3>&1)
+	pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	while ! [ "$pass1" = "$pass2" ]; do
+		unset pass2
+		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
+		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
+	done
+	VMWAREGUEST=$((dialog --inputbox "Are you running in VMware?" 10 60 3>&1 1>&2 2>&3 3>&1)
+	;}
+getuserandpass
 useradd -m -s /bin/bash "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
 echo "$name:$pass" | chpasswd
 unset pass
 usermod -a -G wheel,video "$name"
-# Refresh Arch keyrings
-pacman --noconfirm -Sy archlinux-keyring
-# Upgrade.
-pacman --noconfirm -Syu
-pacman --noconfirm --needed -S base-devel linux-firmware diffutils vim networkmanager man-db man-pages texinfo exfat-utils e2fsprogs
-newperms() {
-	sed -i "/#Deploydot/d" /etc/sudoers
-	echo "$* #Deploydot" >> /etc/sudoers ;}
-
-newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
 # Use all cores for compile
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 # Make pacman and yay nice-looking 
