c8fe5ce39b155efc467daa56ee63391163c357ad
Batresh <bjoernliborius@gmail.com>
2020-03-27 04:04:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -125,21 +125,6 @@ chown -R "$name:wheel" "/home/$name"
 chown "$name:wheel" /usr/bin/mpc
 # Avoid blank screen when setting brigtness
 sudo -u $name light -N 1
-# s6 + s66-rc. Disabled due to having problems creating a dbus session
-#pacman --noconfirm -S lightdm-66serv && pacman --noconfirm -S dbus-66serv consolekit2 consolekit-66serv networkmanager-66serv dhclient-66serv
-#pacman --noconfirm -S boot-user@-66mod
-#66-mods.sh boot-user@$name
-#66-tree -nE boot-user
-#66-enable -t boot-user All-$name && sed -i "\$a[ -f ~/.profile ] && . ~/.profile" /home/$name/.xsession
-#66-tree -ncE desktop
-#66-enable -t desktop dbus consolekit lightdm networkmanager
-#66-disable -t root dhcpcd
-#sudo -u $name 66-tree -nE $name
-#sudo -u $name 66-enable -t $name dbus-session@$name
-# Systemd
-#systemctl enable NetworkManager
-#systemctl enable lightdm
-#systemctl start NetworkManager
 WHICHINIT=$(stat /proc/1/exe | head -1)
 if [[ $WHICHINIT == *runit* ]]; then 
 		dbus-uuidgen >| /etc/machine-id
