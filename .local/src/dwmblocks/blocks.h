9b9b8c2fd42f20ce5721bcb1a93ef6403bfeadc0
batresh <batresh@github.com>
2020-04-17 22:40:20 +0200

    update

--- .local/src/dwmblocks/blocks.h
+++ .local/src/dwmblocks/blocks.h
@@ -1,16 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"bandwidth",	3,	2},
-	{"",	"network",	30,	6},
-	{"",	"volume",	0,	10},
-	{"",	"weather",	10000,	5},
-	{"",	"freespace",	1000,	4},
-	{"",	"ram",		4,	4},
-	{"",	"cpu",		4,	0},
-	{"",	"battery",	5,	0},
-	{"",	"clock",	60,	0},
+	{"",	"dwmupdates",	10000,	0},
+	{"",	"dwmbandwidth",	3,	0},
+	{"",	"dwmlan",	30,	0},
+	{"",	"dwmwan",	30,	0},
+	{"",	"dwmvolume",	0,	10},
+	{"",	"dwmweather",	10000,	0},
+	/*{"",	"dwmfreespace",	1000,	0},*/
+	{"",	"dwmram",	4,	0},
+	{"",	"dwmcpu",	4,	0},
+	{"",	"dwmbattery",	5,	0},
+	{"",	"dwmcorona",	10000,	0},
+	{"",	"dwmclock",	60,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char* delim = " | ";
+static char* delim = "";
