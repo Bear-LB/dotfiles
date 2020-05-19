b9734e7296b6630ac33a2ee25fdc59f659adeb6a
Batresh <bjoernliborius@gmail.com>
2020-05-19 11:20:36 +0200

    Update dwm.c

--- .local/src/dwm/dwm.c
+++ .local/src/dwm/dwm.c
@@ -989,7 +989,7 @@ focusstack(const Arg *arg)
 	int i = stackpos(arg);
 	Client *c, *p;
 
-	if (i < 0 || selmon->sel->isfullscreen)
+	if (i < 0 || !selmon->sel || selmon->sel->isfullscreen)
 		return;
 
 	for(p = NULL, c = selmon->clients; c && (i || !ISVISIBLE(c));
