471f29fa12d48fae8e1d494ad70e7a2aea655e26
Bear-LB <18243015+Bear-LB@users.noreply.github.com>
2025-02-10 16:21:46 +0100

    add dotfiles.sh

--- UbuntuScripts/install-scripts/dotfiles.sh
+++ UbuntuScripts/install-scripts/dotfiles.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 # Clone dotfiles to home
-git clone -b master --depth 1 https://github.com/Bear-LB/dotfiles ~/dotfiles
+git clone -b master --depth 1 https://github.com/Bear-LB/dotfiles
+
+#USER=$(whoami)
+
+ln -sf /home/"$USER"/dotfiles/.config/hypr /home/"$USER"/.config/hypr
+ln -sf /home/"$USER"/dotfiles/.config/hypr /home/"$USER"/.config/hypr
+ln -sf /home/"$USER"/dotfiles/.config/hypr /home/"$USER"/.config/hypr
+ln -sf /home/"$USER"/dotfiles/.config/hypr /home/"$USER"/.config/hypr
+ln -sf /home/"$USER"/dotfiles/.config/hypr /home/"$USER"/.config/hypr
+ln -sf /home/"$USER"/dotfiles/.config/hypr /home/"$USER"/.config/hypr
